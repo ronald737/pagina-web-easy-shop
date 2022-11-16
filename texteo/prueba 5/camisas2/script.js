67649d6d723d908e59b49c217189a7ad85e3ab32
75910485 <112639705+75910485@users.noreply.github.com>
2022-11-16 18:53:15 -0500

    cambios quiero juugar dota

---
+++ texteo/prueba 5/camisas2/script.js
@@ -0,0 +1,32 @@
+/* Youtube Link: https://www.youtube.com/watch?v=i7bW41JTL8U */
+/* Created by Truzz Blogg */
+/* https://www.youtube.com/c/TruzzBlogg/videos */
+
+let redBtn = document.getElementById("red");
+let blueBtn = document.getElementById("blue");
+let yellowBtn = document.getElementById("yellow");
+let greenBtn = document.getElementById("green");
+let brownBtn = document.getElementById("brown");
+let imgchange = document.getElementById("imgchange");
+
+redBtn.onclick = function() {
+
+    imgchange.src = "./images/camisas/Cdiseño2rojo.png";
+}
+
+blueBtn.onclick = function() {
+    imgchange.src = "./images/camisas/Cdiseño2azul.png";
+}
+
+yellowBtn.onclick = function() {
+    imgchange.src = "./images/camisas/Cdiseño2amarillo.png";
+}
+
+greenBtn.onclick = function() {
+    imgchange.src = "./images/camisas/Cdiseño2verde.png";
+}
+
+brownBtn.onclick = function() {
+    imgchange.src = "./images/camisas/Cdiseño2marron.png";
+}
+
---
+++ texteo/prueba 5/pantalones1/script.js
@@ -0,0 +1,32 @@
+/* Youtube Link: https://www.youtube.com/watch?v=i7bW41JTL8U */
+/* Created by Truzz Blogg */
+/* https://www.youtube.com/c/TruzzBlogg/videos */
+
+let redBtn = document.getElementById("red");
+let blueBtn = document.getElementById("blue");
+let yellowBtn = document.getElementById("yellow");
+let greenBtn = document.getElementById("green");
+let brownBtn = document.getElementById("brown");
+let imgchange = document.getElementById("imgchange");
+
+redBtn.onclick = function() {
+
+    imgchange.src = "./images/pantalones/Pdiseño1rojo.png";
+}
+
+blueBtn.onclick = function() {
+    imgchange.src = "./images/pantalones/Pdiseño1azul.png";
+}
+
+yellowBtn.onclick = function() {
+    imgchange.src = "./images/pantalones/Pdiseño1amarillo.png";
+}
+
+greenBtn.onclick = function() {
+    imgchange.src = "./images/pantalones/Pdiseño1verde.png";
+}
+
+brownBtn.onclick = function() {
+    imgchange.src = "./images/pantalones/Pdiseño1marron.png";
+}
+
