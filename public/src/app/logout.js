66ec6c2c8bfc966a060c92745e353e7714fb1138
74768494 <112639656+74768494@users.noreply.github.com>
2022-12-14 20:04:45 -0500

    klk

--- public/src/app/logout.js
+++
@@ -1,8 +0,0 @@
-import { signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"
-import { auth } from './firebase.js'
-const logout = document.querySelector('#logout')
-
-logout.addEventListener('click', async () => {
-    await signOut(auth)
-    console.log("Cerraste sesión")
-})
